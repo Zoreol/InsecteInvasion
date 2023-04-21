521b1ccc18843c9b99d99786944f4f514842c163
Zoreol <105099379+Zoreol@users.noreply.github.com>
2023-04-21 20:29:37 +0200

    debut integration anim

--- Assets/_Scripts/_Ennemi/IAUnitManager.cs
+++ Assets/_Scripts/_Ennemi/IAUnitManager.cs
@@ -12,6 +12,7 @@ public class IAUnitManager : UnityManager
     public List<IAUnitManager> IAUnitManager_List;
     public List<GameObject> playerUnit;
     public Transform formationPoint;
+    
 
     private Vector2 moveto;
     private Bounds bndFloor;
@@ -28,7 +29,17 @@ public class IAUnitManager : UnityManager
 
         StartCoroutine(SetRandomDestination());
     }
-    
+
+    private void Update()
+    {
+        /*
+        if (InFormation)
+        {
+            animUnit.SetTrigger("Attack");
+        }*/
+        
+    }
+
     public void Attack()
     {
         if (Vector2.Distance(this.gameObject.transform.position, playerUnit[0].transform.position) <= 2)
