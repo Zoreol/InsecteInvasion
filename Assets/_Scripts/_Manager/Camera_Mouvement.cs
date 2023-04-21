a3b75ea1b13bef8e64ad39f0437b52f5428aaede
dragonfire23 <dragonfire23@orange.fr>
2023-04-21 15:11:24 +0200

    camera

--- Assets/_Scripts/_Manager/Camera_Mouvement.cs
+++ Assets/_Scripts/_Manager/Camera_Mouvement.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class Camera_Mouvement : MonoBehaviour
 {
     public Camera cam;
+    [SerializeField] float speed;
+    [SerializeField] float zoomspeed;
+    private void Update()
+    {
+        MouveCamera();
+        ZoomCamera();
+    }
+    void ZoomCamera()
+    {
+        if (cam.orthographic)
+        {
+            cam.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoomspeed;
+            if(cam.orthographicSize < 20)
+            {
+                cam.orthographicSize = 20;
+            }
+            if(cam.orthographicSize > 100)
+            {
+                cam.orthographicSize = 100;
+            }
+        }
+    }
     void MouveCamera()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && cam.transform.position.x > -160f)
         {
-            cam.transform.position = new Vector3(cam.transform.position.x - 10 * Time.deltaTime, cam.transform.position.y, cam.transform.position.z);
+            cam.transform.position = new Vector3(cam.transform.position.x - speed * Time.deltaTime, cam.transform.position.y, cam.transform.position.z);
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow) && cam.transform.position.x < 356)
         {
-            cam.transform.position = new Vector3(cam.transform.position.x + 10 * Time.deltaTime, cam.transform.position.y, cam.transform.position.z);
+            cam.transform.position = new Vector3(cam.transform.position.x + speed * Time.deltaTime, cam.transform.position.y, cam.transform.position.z);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) && cam.transform.position.y > -210f)
         {
-            cam.transform.position = new Vector3(cam.transform.position.x , cam.transform.position.y - 10 * Time.deltaTime, cam.transform.position.z);
+            cam.transform.position = new Vector3(cam.transform.position.x , cam.transform.position.y - speed * Time.deltaTime, cam.transform.position.z);
         }
-        else if (Input.GetKey(KeyCode.UpArrow))
+        else if (Input.GetKey(KeyCode.UpArrow) && cam.transform.position.y < 314)
         {
-            cam.transform.position = new Vector3(cam.transform.position.x , cam.transform.position.y + 10 * Time.deltaTime, cam.transform.position.z);
+            cam.transform.position = new Vector3(cam.transform.position.x , cam.transform.position.y + speed * Time.deltaTime, cam.transform.position.z);
         }
     }
 }
