45632382db5b14a1532a99d8ad9f6fea0fabc3d1
dragonfire23 <dragonfire23@orange.fr>
2023-04-21 18:46:50 +0200

    animation mantes synchro

--- Assets/_Scripts/_Mante/Commune_Caracteristique_Mantis.cs
+++ Assets/_Scripts/_Mante/Commune_Caracteristique_Mantis.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Commune_Caracteristique_Mantis : MonoBehaviour
 {
     public  int health;
+    private int _current_health;
     public  int damage;
     public int _maxHealth;
+    public Animator anim;
     private void Awake()
     {
         health = _maxHealth;
+        _current_health = health;
+    }
+    private void Update()
+    {
+        if(health < 0)
+        {
+            anim.SetBool("Dead", true);
+        }
+        if(_current_health != health)
+        {
+            anim.SetBool("subi_Degats", true);
+            _current_health = health;
+            anim.SetBool("subi_Degats", false);
+        }
     }
 
 }
--- Assets/_Scripts/_Mante/Target_Keep.cs
+++ Assets/_Scripts/_Mante/Target_Keep.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Target_Keep : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Target_Keep : MonoBehaviour
     public GameObject _ennemi_to_keep;
     [SerializeField] private Commune_Caracteristique_Mantis ccm;
     [SerializeField] private float _timer_attack;
+    [SerializeField] private Animator anim;
     private void Awake()
     {
         select = FindObjectOfType<Selection>();
@@ -37,6 +39,7 @@ public class Target_Keep : MonoBehaviour
         if(Input.GetMouseButtonDown(1) && select._selected_ennemi_List != null)
         {
             _ennemi_to_keep = null;
+            anim.SetBool("Attack", false);
         }
         
         
@@ -45,18 +48,28 @@ public class Target_Keep : MonoBehaviour
     {
         if (collision.tag == /*_ennemi_to_keep.tag*/ "Ennemi" && _ennemi_to_keep != null)
         {
+            anim.SetBool("Attack", true);
             _ennemi_to_keep.GetComponent<IAUnitManager>().life = _ennemi_to_keep.GetComponent<IAUnitManager>().life - ccm.damage;
             _timer_attack = 2.5f;
         }
-        else if (_ennemi_to_keep == null) return;
+        else if (_ennemi_to_keep == null) 
+        {
+            anim.SetBool("Attack", false);
+            return; 
+        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag == /*_ennemi_to_keep.tag*/ "Ennemi" && _ennemi_to_keep != null && _timer_attack <= 0)
         {
+            anim.SetBool("Attack", true);
             _ennemi_to_keep.GetComponent<IAUnitManager>().life = _ennemi_to_keep.GetComponent<IAUnitManager>().life - ccm.damage;
             _timer_attack = 2.5f;
         }
-        else if(_ennemi_to_keep == null) return;
+        else if (_ennemi_to_keep == null)
+        {
+            anim.SetBool("Attack", false);
+            return;
+        }
     }
 }
