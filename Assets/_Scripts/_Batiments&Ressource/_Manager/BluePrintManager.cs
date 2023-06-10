2534c34ca07752d058d0eb1bc9df89be851c7f05
ThomasSouillard <thomassouillard23@gmail.com>
2023-06-10 13:50:50 +0200

    anim et bonne partie batiment

---
+++ Assets/_Scripts/_Batiments&Ressource/_Manager/BluePrintManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePrintManager : MonoBehaviour
+{
+    [SerializeField] private Material mat_invalid;
+    [SerializeField] private float rotationSpeed = 10;
+    [SerializeField] private List<Building_Datas> datas = new List<Building_Datas>();
+
+    private int _layerMaskTerrain = 1 << 6;
+    private GameObject blueprintGO;
+    private BluePrintCtrl bluePrint;
+    private int dataIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (blueprintGO)
+        {
+            SetMaterial();
+            CursorControlls();
+        }
+    }
+}
